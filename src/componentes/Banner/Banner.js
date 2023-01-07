511d947a62e452f7c381921d25f5c87ab11b276a
Sgtelles <sg.telles@gmail.com>
2023-01-06 21:59:26 -0300

    Subindo o projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return(
+        <header className='banner'>
+            <img src="/image/banner.png" alt="Banner principal da pagina do organo" />
+        </header>
+    )
+}
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,50 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+    const css = { backgroundColor: props.corSecundaria }
+    return (
+       props.colaboradores.length > 0 &&
+        <section
+            className='time'
+            style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores
+                    .map(colaborador =>
+                        <Colaborador
+                            corDeFundo = {props.corPrimaria}
+                            key={colaborador.nome}
+                            nome={colaborador.nome}
+                            cargo={colaborador.cargo}
+                            imagem={colaborador.imagem}
+                        />
+                    )}
+            </div>
+        </section>
+        
+        //é a mesma coisa só que usando o ternário, se true ele mostra, e se false ele não mostra nada
+       /* 
+       props.colaboradores.length > 0 ?
+        <section
+            className='time'
+            style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores
+                    .map(colaborador =>
+                        <Colaborador
+                            nome={colaborador.nome}
+                            cargo={colaborador.cargo}
+                            imagem={colaborador.imagem}
+                        />
+                    )}
+            </div>
+        </section>
+        :''
+        */
+        
+    )
+}
+
+export default Time
